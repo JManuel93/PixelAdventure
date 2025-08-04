9d1c1ff86db85b163dfc121f4f8bd7b556ab538b
JManuel93 <pugamanuel0309@gmail.com>
2025-08-04 12:35:45 -0600

    v04.AGO.2025

--- Assets/Scripts/ParallaxEfecto.cs
+++ Assets/Scripts/ParallaxEfecto.cs
@@ -4,17 +4,23 @@ public class ParallaxEfecto : MonoBehaviour
 {
     public Transform camara; // Cámara principal
     public float velocidadParallax = 0.5f; // Cuánto se mueve esta capa
-    private Vector3 posicionInicial;
+    private float parallaxFactor;
+    private float inicioX;
+    private float anchoSprite;
 
     void Start()
     {
-        posicionInicial = transform.position;
+        inicioX = transform.position.x;
+        anchoSprite = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float temp = (camara.position.x * (1 - parallaxFactor));
         float distancia = camara.position.x * velocidadParallax;
-        transform.position = new Vector3(posicionInicial.x + distancia, posicionInicial.y, posicionInicial.z);
+        transform.position = new Vector3(inicioX + distancia, transform.position.y, transform.position.z);
+        if (temp > inicioX + anchoSprite) inicioX += anchoSprite;
+        else if (temp < inicioX - anchoSprite) inicioX -= anchoSprite;
     }
 }
